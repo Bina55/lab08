0236ee20f2372ea3ddf65e5fb9944aa018600a28
Sabrina <sabrina.tassinari@studio.unibo.it>
2024-12-27 18:11:22 +0100

    Exercise 83 done

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberOutputImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberOutputImpl.java
@@ -6,22 +6,34 @@ import it.unibo.mvc.api.DrawResult;
 
 public class DrawNumberOutputImpl implements DrawNumberView {
 
+    public static String NEW_GAME = " a new game starts!";
+
+    private DrawNumberController controller;
+
     @Override
-    public void setController(DrawNumberController observer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    public void setController(final DrawNumberController observer) {
+        this.controller = observer;
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 
     @Override
     public void result(DrawResult res) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'result'");
+        switch (res) {
+            case YOURS_HIGH, YOURS_LOW:
+                System.out.println(res.getDescription());
+                return;
+            case YOU_WON: 
+                System.out.println(res.getDescription() + NEW_GAME);
+                break;
+            case YOU_LOST:
+                System.out.println(res.getDescription() + NEW_GAME);
+                break;
+            default:
+                throw new IllegalStateException("Unknow status game");
+        }
     }
     
 }
